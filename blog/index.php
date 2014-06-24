271b9f69f933697b75782e5e34bef4d359f300e3
jinshan <jinshan.sb@taobao.com>
2014-06-25 07:02:51 +0800

    up

--- blog/index.php
+++ blog/index.php
@@ -12,7 +12,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
-// p(Yii::app()->db);
+p(Yii::app()->cache);
 
 
 
--- test.php
+++ test.php
@@ -1,2 +1,5 @@
 <?php
-   echo "hello world"; 
+    ini_set ('display_errors', 'on');
+    ini_set ('error_reporting', E_ALL);
+    ini_set ('log_errors', 'off');
+    echo phpinfo();
